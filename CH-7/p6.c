d607633fc08924cd304a8c4e5b4c7f30c0b7dc7c
JoshuaC92 <cukurinsjosh@gmail.com>
2023-06-23 22:41:54 +1000

    23JUN23

---
+++ CH-7/p6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(short));
+	printf("%lu\n", sizeof(long));
+	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(double));
+	printf("%lu\n", sizeof(long double));
+
+	return 0;
+}
