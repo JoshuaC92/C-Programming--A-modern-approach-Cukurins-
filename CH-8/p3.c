4fbe69b1cf1d0d0155f842bec3766bc53e452aa9
JoshuaC92 <cukurinsjosh@gmail.com>
2023-07-06 21:09:52 +1000

    06JUL23#2

--- CH-8/p3.c
+++ CH-8/p3.c
@@ -7,23 +7,30 @@ int main(void){
 	bool digit_seen[10] = {false}; 		// Keep track of all decimal digits (0 - 9)
 	long n;					// Store number entered by user (line 12)
 	int digit;				// Store digit taken from n
+	int num_count;				// count the number of input numbers from user
 
 	printf("Enter a number: ");
 	scanf("%ld", &n);
 
 	while (n > 0){
-		digit = n % 10;			//get the least significant (right most) digit 
-		if (digit_seen[digit]){		//check if the digit has been seen already
-			break;			//Exit loop as duplicate encountered
+		while (n > 0){			//Old while loop will perform the normal operation
+			digit = n % 10;		//get the least significant (right most) digit 
+			if (digit_seen[digit]){	//check if the digit has been seen already
+				break;		//Exit loop as duplicate encountered
+				};
+			digit_seen[digit] = true;	//mark the digit as seen. 
+			n /= 10;		// remove the lease significant digit
+			}
+		if (n > 0){			//Check if while (line 14) broke early
+			printf("Repeated digit.\n");
+			}
+		else printf("No repeated digit.\n");
+		for(int i = 0; i < 10; i++){
+			digit_seen[i] = false;  	
 			};
-		digit_seen[digit] = true;	//mark the digit as seen. 
-		n /= 10;			// remove the lease significant digit
-		}
-	
-	if (n > 0){				//Check if while (line 14) broke early
-		printf("Repeated digit.\n");
-		}
-	else printf("No repeated digit.\n");
+		printf("Enter a number: ");	
+		scanf("%ld", &n);
+		};
 
 	return 0;
 }
