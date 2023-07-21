feeae86ce241fd3754e87cc568251888d34ba68a
JoshuaC92 <cukurinsjosh@gmail.com>
2023-07-21 20:36:23 +1000

    21JUL23

--- CH-8/p15.c
+++ CH-8/p15.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void){
 	char msg[80];
 	char ch;
-	int endflag;
+	int endflag = 0;
 	int shift;
 	int length;
-
+	
 	printf("Enter a message to be encrypted: ");
 
 	for (int i = 0; endflag == 0; i++){
@@ -19,16 +20,23 @@ int main(void){
 		msg[i] = ch;
 		};
 	
-	for (int i = 0; i <= length; i++){
-		printf("%c", msg[i]);
-		};
-	
 	printf("\n");
 
 	printf("Enter a shift amount: ");
 	scanf("%d", &shift);
 
 	printf("Encrypted message: ");
+	
+	for (int i = 0; i <= length; i++){
+		ch = msg[i]; 
+		if ( islower(ch) ){ 
+			printf("%c", ((ch - 'a') + shift)  % 26 + 'a');
+			}
+		else if ( isupper(ch) ){
+			printf("%c", ((ch - 'A') + shift)  % 26 + 'A');
+			}
+		else printf("%c", ch);
+		};
 
 	printf("\n");
 
