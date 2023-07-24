4996b05bcea081377083e13a19327287cb6d1d89
JoshuaC92 <cukurinsjosh@gmail.com>
2023-07-24 20:42:18 +1000

    24JUL23#2

--- CH-8/p17.c
+++ CH-8/p17.c
@@ -1,23 +1,59 @@
 #include <stdio.h>
 
 int main(void){
-	int n;
+	int n, count, oldRow, oldCol;
 	int row = 0;
 
 	printf("Enter a odd number between 1 and 99: ");
 	scanf("%d", &n);
 
 
-	for (row = 0; row < n; row++) {
-        	for (col = 0; col < n; col++) {
-            		square[row][col] = 0;
-        		};
-    		};
-
 	int square[n][n];
 	int col = n / 2;
 	square[row][col] = 1;
-
 	
+	for (int i = 0; i < n; i++) {
+       		for (int i2 = 0; i2 < n; i2++) {
+           		square[i][i2] = 0;
+        		};
+    		};
+
+
+	row = n / 2; 
+    	col = 0;
+    	square[row][col] = 1;
+
+    	count = 2;
+    	while (count <= n * n) {
+        	oldRow = row;
+        	oldCol = col;
+        	if (oldRow + 1 >= n){
+            		row = 0;
+			}
+        	else row = oldRow + 1;
+        	
+		if (oldCol - 1 < 0){
+            		col = n - 1;
+			}
+        	else col = oldCol - 1;
+        
+		if (square[row][col] == 0) {
+            		square[row][col] = count++;
+        		} 	
+		else {
+            		if (oldCol + 1 >= n){
+                		square[row = oldRow][col = 0] = count++;
+				}
+            		else square[row = oldRow][col = oldCol + 1] = count++;
+                        }
+		};
+
+    	for (col = 0; col < n; col++) {
+        	for (row = 0; row < n; row++) {
+            		printf("%4d", square[row][col]);
+        		};
+        	printf("\n");
+    		}
+    
 	return 0;
-}		
+}
