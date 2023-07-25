477f08405eb210c7e67455ee72321bbdb5067f50
JoshuaC92 <cukurinsjosh@gmail.com>
2023-07-25 20:24:02 +1000

    25JUL23#3

--- CH-8/test.c
+++ CH-8/test.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
 int main(void){
-	printf("HERE IS A MOD DEMO:      ");
+	int count = 0;
 
-	printf("%d", 7 % (1 - 1));
-
-	printf("\n");
-	printf("HERE IS A MOD DEMO:      %d", 7 % (7+1));
-
-	printf("\n answes should be 7 and 1 \n");
-
+	printf("%d\n", count++);
+	
 	return 0;
 }
