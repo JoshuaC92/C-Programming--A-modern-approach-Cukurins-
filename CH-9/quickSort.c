5dc8f8f04356bdafcd64676d17d57cfe2adc403b
JoshuaC92 <cukurinsjosh@gmail.com>
2023-07-27 21:01:55 +1000

    27JUL23

---
+++ CH-9/quickSort.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+	int a[7] = {5,7,6,3,2,4,1};
+	int low = 0;
+	int high = 6;
+	int hole = a[low];
+
+	for (int i = 0;
+
---
+++ CH-9/simpleRecursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int fact(int n){
+	if (n <= 1){
+		return 1;
+		}
+	else
+		return n * fact(n - 1);
+}
+
+int main(void){
+	printf("%d\n", fact(5));
+} 
