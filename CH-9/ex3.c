76acc8564fce45a7a4cdc845ced23067faab46df
JoshuaC92 <cukurinsjosh@gmail.com>
2023-08-03 21:06:45 +1000

    ex3.c

---
+++ CH-9/ex3.c
@@ -0,0 +1,12 @@
+// Write a function, gcd(m, n) that calculates the greatest common divisor of the integers m and n.
+// (Programming program 2 in chapter 6 describes Euclids algorithm for computing the GCD.)
+
+#include <stdio.h>
+
+int  gcd(int m, int n);
+
+int main(void){
+	return 0;
+}
+
+int  gcd(int m, int n);
