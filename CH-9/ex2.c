9a87458d1e47cd00d61cf66adf0e805dc3bcaa62
JoshuaC92 <cukurinsjosh@gmail.com>
2023-08-03 20:55:12 +1000

    Exercise 2.

---
+++ CH-9/ex2.c
@@ -0,0 +1,13 @@
+// Write a function 'check(x, y, n,)' that returns 1 if both x and y fall between 0 and n-1, 
+// inclusive. The function should retun 0 otherwise. Assume that x, y and n are all of type int.
+
+#include <stdio.h>
+
+int check(int x, int n, int y);
+
+int main(void){
+	return 0;
+}
+
+int check(int x, int n, int y){
+	
